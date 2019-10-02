f1fc138de1864a974ce6581b85bf01af214078cd
radville <laura.radville@gmail.com>
2019-10-02 10:20:19 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#home'
+
+  # resources :secrets
+  get '/secrets/show'
+  get 'sessions/new' 
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
 end
